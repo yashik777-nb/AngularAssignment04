fe6326855c56f1b933feca6925a84a070a106fe2
Yash <yaswanth.iddibani@gmail.com>
2021-05-31 22:08:02 +0100

    Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IssuesComponent } from './issues/issues.component';
+import { IssueAddedComponent } from './issues/issue-added/issue-added.component';
+
+import { IssuesService } from './issues/issues.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'issues', component: IssuesComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssuesComponent,
+    IssueAddedComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [IssuesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
